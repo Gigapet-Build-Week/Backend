a286a5b123f10da4b28f17eba4064b6aed984610
BPitts8019 <bradleyj.pitts@gmail.com>
2020-01-31 21:03:33 -0700

    Migrations complete; on to seeding

--- data/migrations/20200131185456_create-tables.js
+++ data/migrations/20200131185456_create-tables.js
@@ -5,9 +5,9 @@ const FOOD_ENTRY = "FOOD_ENTRY";
 const CATEGORY = "CATEGORY";
 
 exports.up = async function (knex) {
-   knex.schema.create_table(USER, table => {
+   await knex.schema.createTable(USER, table => {
       //id  integer [pk, increment] //auto increment
-      table.increment("id");
+      table.increments("id");
       //username varchar(128) [not null, unique]
       table.string("username", 128)
          .unique()
@@ -28,9 +28,9 @@ exports.up = async function (knex) {
          .notNullable();
    });
 
-   knex.schema.create_table(CHILD, table => {
+   await knex.schema.createTable(CHILD, table => {
       //id  integer [pk, increment] //auto increment
-      table.increment("id"); 
+      table.increments("id"); 
       //parent_id integer [not null, ref: > user.id]
       table.integer("parent_id") 
          .notNullable()
@@ -47,9 +47,9 @@ exports.up = async function (knex) {
 
    });
 
-   knex.schema.create_table(PET, table => {
+   await knex.schema.createTable(PET, table => {
       //id  integer [pk, increment] //auto increment
-      table.increment("id");
+      table.increments("id");
       //id  integer [pk, increment] //auto increment
       table.integer("child_id")
          .unique()
@@ -62,16 +62,16 @@ exports.up = async function (knex) {
          .unsigned()
          .notNullable()
          .defaultTo(0);
-      //health_total_target unsigned [default: 500]
-      table.integer("health_total")
+      //health_target unsigned [default: 500]
+      table.integer("health_target")
          .unsigned()
          .notNullable()
          .defaultTo(500);
    });
 
-   knex.schema.create_table(CATEGORY, table => {
+   await knex.schema.createTable(CATEGORY, table => {
       //id  integer [pk, increment] //auto increment
-      table.increment("id");
+      table.increments("id");
       //name string [not null, unique]
       table.string("name", 128)
          .unique()
@@ -80,12 +80,12 @@ exports.up = async function (knex) {
       table.integer("suggested_servings")
          .defaultTo(1);
       //description string [default: null]
-      table.integer("suggested_servings")
+      table.integer("description")
    });
 
-   knex.schema.create_table(FOOD_ENTRY, table => {
+   await knex.schema.createTable(FOOD_ENTRY, table => {
       //id  integer [pk, increment] //auto increment
-      table.increment("id");
+      table.increments("id");
       //child_id integer [ref: > child.id]
       table.integer("child_id")
          .notNullable()
@@ -100,19 +100,19 @@ exports.up = async function (knex) {
       //eaten_on date [not null]
       table.date("eaten_on")
          .notNullable();
-      //suggested_servings unsigned [default: 1]
-      table.integer("suggested_servings")
-         .unsigned()
-         .defaultTo(1);
       //description string
       table.string("description")
+      //servings unsigned [default: 1]
+      table.integer("servings")
+         .unsigned()
+         .defaultTo(1);
    });
 };
 
 exports.down = async function (knex) {
-   knex.schema.dropTableIfExists(FOOD_ENTRY);
-   knex.schema.dropTableIfExists(CATEGORY);
-   knex.schema.dropTableIfExists(PET);
-   knex.schema.dropTableIfExists(CHILD);
-   knex.schema.dropTableIfExists(USER);
+   await knex.schema.dropTableIfExists(FOOD_ENTRY);
+   await knex.schema.dropTableIfExists(CATEGORY);
+   await knex.schema.dropTableIfExists(PET);
+   await knex.schema.dropTableIfExists(CHILD);
+   await knex.schema.dropTableIfExists(USER);
 };
