282a824313ca4ef8c6ed7e1041d79c2df3770b84
BPitts8019 <bradleyj.pitts@gmail.com>
2020-02-02 18:51:54 -0700

    getting closer

--- data/seeds/01-users.js
+++ data/seeds/01-users.js
@@ -4,25 +4,19 @@ exports.seed = async knex => {
          username: "Parent 01", 
          password: "plain text for now", 
          is_onboarded: 0,
-         knickname: "Mom",
-         created_at: "2019-11-13 04:23:00",
-         last_login: "2020-01-31 20:09:00"
+         knickname: "Mom"
       },
       {
          username: "Parent 02", 
          password: "plain text for now", 
          is_onboarded: 1,
-         knickname: "Dad",
-         created_at: "2020-01-01 07:89:10",
-         last_login: "2020-01-12 08:09:00"
+         knickname: "Dad"
       },
       {
          username: "Parent 03", 
          password: "plain text for now", 
          is_onboarded: 0,
-         knickname: "Billy Bob",
-         created_at: "2019-11-13 04:23:00",
-         last_login: "2019-12-31 23:59:59"
+         knickname: "Billy Bob"
       }
    ]);
 };
