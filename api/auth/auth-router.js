a41b7bea0c640e5d1dbe33aec8098b52d3be7681
BPitts8019 <bradleyj.pitts@gmail.com>
2020-02-02 22:42:24 -0700

    Minor tweak to the auth router.

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -26,8 +26,8 @@ router.post("/register", async (req, res, next) => {
       }
 
       //register user
-      const newUser = await authModel.addUser(req.body);
-      const {password, ...sanitizedUser} = newUser[0];
+      const [newUser] = await authModel.addUser(req.body);
+      const {password, ...sanitizedUser} = newUser;
       sanitizedUser.is_onboarded = !!sanitizedUser.is_onboarded;
 
       //return sanitized user data
