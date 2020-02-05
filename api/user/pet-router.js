d41926336722357052b134be60bec3343c7a0e03
BPitts8019 <bradleyj.pitts@gmail.com>
2020-02-04 22:20:32 -0700

    Added different middleware, but awesome

--- api/user/pet-router.js
+++ api/user/pet-router.js
@@ -1,12 +1,15 @@
 const router = require("express").Router({mergeParams: true});
-const inputValidation = require("../middleware/inputValidation");
 const insertRecord = require("../utils/insertRecord");
 const {status, msg, tableNames, tables: {Pets, Children}} = require("../constants");
 
-//`POST /api/users/children/:id/pet
-router.post("/", async (req, res, next) => {
-   //ID must be integer greater than 0
+const validateInput = (req, res, next) => {
+   //Child ID must be an integer greater than 0
    const id = Number(req.params.id);
+   if (!Number.isInteger(id)) {
+      return res.status(status.BAD_REQ).json({
+         message: msg.BAD_PET_DATA
+      });
+   }
    if (!id || id < 1) {
       return res.status(status.BAD_REQ).json({
          message: msg.BAD_PET_DATA
@@ -21,7 +24,7 @@ router.post("/", async (req, res, next) => {
       });
    }
    //health and heath_target must be integers greater-than or equal to 0
-   if (!Number.isInteger(health) || !Number.isInteger(health_target) ||
+   if (!Number.isInteger(Number(health)) || !Number.isInteger(Number(health_target)) ||
       (health < 0 || health_target < 0))
    {
       return res.status(status.BAD_REQ).json({
@@ -29,19 +32,60 @@ router.post("/", async (req, res, next) => {
       });
    }
 
+   req.newPet = {child_id: id, health, health_target};
+   next();
+};
+const ChildMustExist = async (req, res, next) => {
+   try {
+      //Child must exist
+      const [child] = await Children.findById(req.newPet.child_id);
+      if (!child) {
+         return res.status(status.NOT_FOUND).json({
+            message: msg.NO_CHILD_EXISTS
+         });
+      }
+   
+      req.child = child;
+      next();
+   } catch (error) {
+      next(error);
+   }
+};
+
+const MustBeAllowed = (req, res, next) => {
+   //parent must 'own' the child
+   console.log(`${req.tokenPayload.id} !== ${req.child.parent_id}`);
+   if (req.tokenPayload.id !== req.child.parent_id) {
+      return res.status(status.FORBIDDEN).json({
+         message: msg.FORBIDDEN
+      });
+   }
+
+   next();
+};
+const noDuplicatePets = async (req, res, next) => {
    try {
       //Must not already exist
-      const [dupePet] = await Pets.findBy({child_id: id});
+      const [dupePet] = await Pets.findBy({child_id: req.child.id});
       if (dupePet) {
          return res.status(status.BAD_REQ).json({
             message: msg.PET_EXISTS
          });
       }
+   
+      next();
+   } catch (error) {
+      next(error);
+   }
+};
 
+
+
+//`POST /api/users/children/:id/pet
+router.post("/", validateInput, ChildMustExist, MustBeAllowed, noDuplicatePets, async (req, res, next) => {
+   try {
       //create a pet avatar
-      const newPet = {child_id: id, health, health_target};
-      const [pet] = await insertRecord(Pets, newPet);
-
+      const [pet] = await insertRecord(Pets, req.newPet);
       if (!pet) {
          throw new Error("Something terrible happend while adding a pet!");
       }
@@ -93,6 +137,9 @@ router.get("/", async (req, res, next) => {
 });
 
 //PUT /api/users/children/:id/pet
+router.put("/", async (req, res, next) => {
+
+});
 
 //// DELETE /api/users/children/:id/pet
 module.exports = router;
