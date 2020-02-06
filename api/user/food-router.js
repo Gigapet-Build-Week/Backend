17a5ff0005a5604fa81d3484b2d94655c1aa07ac
BPitts8019 <bradleyj.pitts@gmail.com>
2020-02-05 21:21:49 -0700

    GET and POST to the food-log is working

--- api/user/food-router.js
+++ api/user/food-router.js
@@ -7,14 +7,19 @@
 - [ ] `DELETE /api/users/children/:id/food-log/:food_id`
 */
 const router = require("express").Router({mergeParams: true});
-const {tables: {Children, Food_entries, Categories}, msg, status} = require("../constants");
+const insertRecord = require("../utils/insertRecord");
+const {
+   tableNames: {CHILDREN, FOOD_ENTRIES},
+   tables: {Children, Food_entries, Categories}, 
+   msg, status
+} = require("../constants");
 
 //local middleware
 const validateId = (idType) => {
    return (req, res, next) => {
       const {id: child_id, food_id} = req.params;
       //ID must be an integer greater than 0
-      const id = Number((idType === "children")? child_id : food_id);
+      const id = Number((idType === CHILDREN)? child_id : food_id);
       if (!Number.isInteger(id)) {
          return res.status(status.BAD_REQ).json({
             message: msg.BAD_FOOD_DATA
@@ -26,27 +31,9 @@ const validateId = (idType) => {
          });
       }
 
-      req.newFoodEntry = (idType === "children")? {child_id: id} : {id};
+      req.newFoodEntry = (idType === CHILDREN)? {child_id: id} : {id};
       next();
    };
-};
-const CategoryMustExist = async (req, res, next) => {
-   const name = req.newFoodEntry.category;
-   
-   try {
-      console.log(`Category Name: ${name}`)
-      const [category] = await Categories.findBy({name});
-      if (!category) {
-         return res.status(status.NOT_FOUND).json({
-            message: msg.NO_CAT_EXISTS
-         });
-      }
-   
-      req.category = category;
-      next();
-   } catch (error) {
-      next(error);
-   }
 };
 const ChildMustExist = async (req, res, next) => {
    try {
@@ -65,7 +52,27 @@ const ChildMustExist = async (req, res, next) => {
       next(error);
    }
 };
+const CategoryMustExist = async (req, res, next) => {
+   const name = req.body.category;
+   
+   try {
+      console.log(`Category Name: ${name}`)
+      const [category] = await Categories.findBy({name});
+      if (!category) {
+         return res.status(status.NOT_FOUND).json({
+            message: msg.NO_CAT_EXISTS
+         });
+      }
+   
+      req.newFoodEntry.category_id = category.id;
+      next();
+   } catch (error) {
+      next(error);
+   }
+};
+
 const validateInput = (req, res, next) => {
+   const newFoodEntry = req.body;
    const {eaten_on, description, servings} = req.body;
 
    //eaton_on must be a Date
@@ -76,6 +83,7 @@ const validateInput = (req, res, next) => {
          message: msg.BAD_FOOD_DATA
       });
    }
+
 
    //transform description to prevent XXS attacks
    // ??
@@ -93,16 +101,22 @@ const validateInput = (req, res, next) => {
       });
    }
 
+   const {category, ...foodEntry} = newFoodEntry;
+   req.newFoodEntry = {
+      ...req.newFoodEntry,
+      ...foodEntry
+   };
    next();
 };
 const FoodMustExist = async (req, res, next) => {
+   const {id} = req.newFoodEntry;
    try {
       //record must exist
-      console.log(`Food ID: ${req.newFoodEntry.id}`)
-      const [food] = await Food_entries.findById(req.newFoodEntry.id);
+      console.log(`Food ID: ${id}`)
+      const [food] = await Food_entries.findById(id);
       if (!food) {
          return res.status(status.NOT_FOUND).json({
-            message: msg.NO_FOOD_EXISTS
+            message: msg.NO_FOOD_LOG
          });
       }
    
@@ -126,18 +140,52 @@ const mustBeAllowed = (req, res, next) => {
 
 //routes
 // /api/users/children/:id/food-log
-router.post("/", validateId("children"), ChildMustExist, validateInput, mustBeAllowed, (req, res, next) => {
+router.post("/", validateId(CHILDREN), ChildMustExist, CategoryMustExist, validateInput, mustBeAllowed, async (req, res, next) => {
+   try {
+      //create a food entry
+      console.log(req.newFoodEntry);
+      const [food] = await insertRecord(Food_entries, req.newFoodEntry);
+      if (!food) {
+         throw new Error("Something terrible happend while adding a pet!");
+      }
+
+      res.status(status.CREATED).json(food);
+   } catch (error) {
+      next(error);
+   }
+});
+router.get("/", validateId(CHILDREN), ChildMustExist, mustBeAllowed, async (req, res, next) => {
+   try {
+      const food_log = await Food_entries.findBy({child_id: req.child.id});
+      if (!food_log || food_log.length === 0) {
+         return res.status(status.NOT_FOUND).json({
+            message: msg.NO_FOOD_LOG
+         });
+      }
+
+      const response = {
+         ...req.child,
+         food_log
+      };
+      res.json(response);
+   } catch (error) {
+      next(error);
+   }
+});
+router.get("/", validateInput, (req, res, next) => {
    res.status(status.BAD_REQ).json({
       message: "endpoint still under construction!!"
    });
 });
-// router.get("/", validateInput, (req, res, next) => {
-//    res.status(status.BAD_REQ).json({
-//       message: "endpoint still under construction!!"
-//    });
-// });
-// router.get();
-// router.put();
-// router.delete();
+router.put("/", validateInput, (req, res, next) => {
+   res.status(status.BAD_REQ).json({
+      message: "endpoint still under construction!!"
+   });
+});
+router.delete("/", validateInput, (req, res, next) => {
+   res.status(status.BAD_REQ).json({
+      message: "endpoint still under construction!!"
+   });
+});
 
 module.exports = router;
