9b337fd8772f15d59b8aed789b57a3c40a0b3a67
BPitts8019 <bradleyj.pitts@gmail.com>
2020-02-06 18:21:55 -0700

    Reworked the middleware to be more efficient

--- api/user/child-router.js
+++ api/user/child-router.js
@@ -4,6 +4,53 @@ const foodRouter = require("./food-router");
 const insertRecord = require("../utils/insertRecord");
 const {status, msg, tables: {Children}} = require("../constants");
 
+const validateChildId = (req, res, next) => {
+   const id = Number(req.params.id);
+
+   //Child ID must be an integer greater than 0
+   if (!Number.isInteger(id)) {
+      return res.status(status.BAD_REQ).json({
+         message: msg.BAD_PET_DATA
+      });
+   }
+   if (!id || id < 1) {
+      return res.status(status.BAD_REQ).json({
+         message: msg.BAD_PET_DATA
+      });
+   }
+
+   next();
+};
+const ChildMustExist = async (req, res, next) => {
+   const id = Number(req.params.id);
+
+   try {
+      //Child must exist
+      const [child] = await Children.findById(id);
+      if (!child) {
+         return res.status(status.NOT_FOUND).json({
+            message: msg.NO_CHILD_EXISTS
+         });
+      }
+   
+      req.child = child;
+      next();
+   } catch (error) {
+      next(error);
+   }
+};
+const mustBeAllowed = (req, res, next) => {
+   //parent must 'own' the child
+   console.log(`${req.tokenPayload.id} !== ${req.child.id}`);
+   if (req.tokenPayload.id !== req.child.id) {
+      return res.status(status.FORBIDDEN).json({
+         message: msg.FORBIDDEN
+      });
+   }
+
+   next();
+};
+
 //`POST /api/users/children`
 router.post("/", async (req, res, next) => {
    //req.tokenPayload has user id and knickname
@@ -58,10 +105,18 @@ router.post("/", async (req, res, next) => {
    }
 });
 
+//`GET /api/users/children/:child_id`
+router.get("/:id", validateChildId, mustBeAllowed, ChildMustExist, async (req, res, next) => {
+
+});
+
+- [ ] `PUT /api/users/children/:child_id`
+- [ ] `DELETE /api/users/children/:child_id`
+
 ///api/users/children/:id/pet
-router.use("/:id/pet", petRouter);
+router.use("/:id/pet", validateChildId, mustBeAllowed, ChildMustExist, petRouter);
 
 ///api/users/children/:id/food-log
-router.use("/:id/food-log", foodRouter);
+router.use("/:id/food-log", validateChildId, mustBeAllowed, ChildMustExist, foodRouter);
 
 module.exports = router;
